47e34fca458f54119d553c535288f3440556e0cf
Gustavo Siqueira <gustavosiqueira2004@gmail.com>
2025-06-09 21:01:45 -0300

    Livro.cs

--- Livro.cs
+++ Livro.cs
@@ -1,13 +1,29 @@
+using System;
+
 public class Livro
 {
     public string Titulo { get; set; }
     public string Autor { get; set; }
-    public int Quantidade { get; set; }
+
+    private int quantidade;
+    public int Quantidade
+    {
+        get => quantidade;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Quantidade não pode ser negativa.");
+            quantidade = value;
+        }
+    }
 
     public Livro(string titulo, string autor, int quantidade = 1)
     {
-        Titulo = titulo;
-        Autor = autor;
+        if (quantidade < 0)
+            throw new ArgumentException("Quantidade não pode ser negativa.");
+
+        Titulo = titulo ?? throw new ArgumentNullException(nameof(titulo));
+        Autor = autor ?? throw new ArgumentNullException(nameof(autor));
         Quantidade = quantidade;
     }
 
